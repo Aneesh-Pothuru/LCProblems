50482b51be3ffd7755b16c35d2885c6fa4ee7e1b
Aneesh Pothuru <pothuruaneesh23@gmail.com>
2021-02-28 00:44:00 -0800

    Problem seven solved

---
+++ Problems/7Solution.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Solution {
+    public int reverse(int x) {
+        long rev = 0;
+        while(x != 0){
+            rev = (rev * 10) + (x % 10);
+            x /= 10;
+        }
+        rev = rev < Integer.MIN_VALUE || rev > Integer.MAX_VALUE ? 0 : rev;
+        return (int) rev;
+    }
+}
